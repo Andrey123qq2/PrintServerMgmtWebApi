95b5e175fd0edb87c42c8ff1b9cce92defa42828
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-02-13 15:26:23 +0200

    feat: added class WmiInvokeResult

---
+++ Domain/Printers/WmiInvokeResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Printers
+{
+    public class WmiInvokeResult
+    {
+        static readonly Dictionary<uint, string> _resultMap = new Dictionary<uint, string>
+        {
+            { 0,  "Successful completion"},
+            { 2,  "The user does not have access to the requested information"},
+            { 5,  "Access Denied"},            
+            { 1801,  "Invalid Printer Name"},
+            { 10000,  "No such entity"},
+            { 10001,  "Entity already in the desired state"},
+        };
+
+        public static string GetDescription(uint code)
+        {
+            if (!_resultMap.TryGetValue(code, out string name))
+            {
+                // Error handling here
+            };
+            return name;
+        }
+    }
+}
