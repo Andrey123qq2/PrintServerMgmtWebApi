9a2be806ffedf09292b4bba75b7de582144f8d81
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-02-16 12:31:52 +0200

    feat: added ErrorHandlerMiddleware

---
+++ API/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionMessageAsync(context, exception).ConfigureAwait(false);
+            }
+        }
+
+        private Task HandleExceptionMessageAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = ErrorHandlerStatusCodeFactory.Create(exception.GetType());
+            var result = JsonConvert.SerializeObject(new
+            {
+                ErrorMessage = exception.Message
+            });            
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
