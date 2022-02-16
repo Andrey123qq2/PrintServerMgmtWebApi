a2df0f5960babf8a781187c3a440373fa87818df
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-02-16 12:34:43 +0200

    feat: added PrinterNotFoundException

---
+++ Infrastructure/PrintServer/PrinterNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Infrastructure.PrintServer
+{
+    public class PrinterNotFoundException : Exception
+    {
+        public PrinterNotFoundException()
+        {
+        }
+        public PrinterNotFoundException(string message)
+            : base(message)
+        {
+        }
+        public PrinterNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
