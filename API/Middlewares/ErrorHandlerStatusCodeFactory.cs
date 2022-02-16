8c57b3594dfdf5c8a4e93302e4461ad7e48a528a
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-02-16 12:32:51 +0200

    feat: added ErrorHandlerStatusCodeFactory

---
+++ API/Middlewares/ErrorHandlerStatusCodeFactory.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.PrintServer;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace API.Middlewares
+{
+    class ErrorHandlerStatusCodeFactory
+    {
+        private static readonly Dictionary<Type, int> _mapExceptionToStatusCode = new Dictionary<Type, int>
+            {
+                {  typeof(PrinterNotFoundException), StatusCodes.Status400BadRequest },
+                {  typeof(UnauthorizedAccessException), StatusCodes.Status403Forbidden },
+            };
+        public static int Create(Type type)
+        {
+            if (!_mapExceptionToStatusCode.ContainsKey(type))
+                return StatusCodes.Status500InternalServerError;
+            return _mapExceptionToStatusCode[type];
+        }
+
+    }
+}
