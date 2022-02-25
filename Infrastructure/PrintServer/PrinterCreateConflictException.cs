e6afd7c8e19c587039edd9c23dc6a6590bae7d99
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-02-25 09:30:46 +0200

    feat: added class PrinterCreateConflictException

---
+++ Infrastructure/PrintServer/PrinterCreateConflictException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.PrintServer
+{
+    public class PrinterCreateConflictException : Exception
+    {
+        public PrinterCreateConflictException()
+        {
+        }
+        public PrinterCreateConflictException(string message)
+            : base(message)
+        {
+        }
+        public PrinterCreateConflictException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
