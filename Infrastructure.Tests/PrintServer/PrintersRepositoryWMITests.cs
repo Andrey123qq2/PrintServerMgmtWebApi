50b73080f9feb79c398382a2750b7b107f22cb51
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-02-25 09:37:24 +0200

    tests: PrintersRepositoryWMITests - added using of propertyFilter

--- Infrastructure.Tests/PrintServer/PrintersRepositoryWMITests.cs
+++ Infrastructure.Tests/PrintServer/PrintersRepositoryWMITests.cs
@@ -25,9 +25,12 @@ namespace PrintServerMgmtWebApi.Repository.Tests
         {
             string printerName = "PRN-TEST-API";
             string shareName = "PRN-TEST-API-SHARE";
+            string printerHostName = "PPRN-TEST-API";
             string driverName = "HP Universal Printing PCL 6";
             string location = "Test Office";
-            var result = _repository.Create(printerName, shareName, driverName, location);
+            string propertyFilter = "PortName";
+            string comment = "123";
+            var result = _repository.Create(printerName, shareName, printerHostName, driverName, location, propertyFilter, comment);
             StringAssert.Contains("DeviceID=\"" + printerName, result.ToString());
         }
 
@@ -35,9 +38,6 @@ namespace PrintServerMgmtWebApi.Repository.Tests
         public void DeleteTest()
         {
             string printerName = "PRN-TEST-API-DELETE";
-            string shareName = "PRN-TEST-API-SHARE-DELETE";
-            string driverName = "HP Universal Printing PCL 6";
-            string location = "Test Office-DELETE";
             //_repository.Create(printerName, shareName, driverName, location);
             Assert.DoesNotThrow(() => _repository.Delete(printerName));
         }
@@ -45,47 +45,51 @@ namespace PrintServerMgmtWebApi.Repository.Tests
         [Test()]
         public void GetTest()
         {
-            string printerName = "PRN-TEST-API";
-            var result = _repository.Get(printerName);
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
+            var result = _repository.Get(printerHostName, propertyFilter);
             if (result == null)
-                Assert.Fail("Expected printer {0} has not been found", printerName);
+                Assert.Fail("Expected printer {0} has not been found", printerHostName);
             else
-                StringAssert.Contains("DeviceID=\"" + printerName, result.ToString());
+                StringAssert.Contains("DeviceID=\"" + printerHostName, result.ToString());
         }
 
         [Test()]
         public void RenamePrinterTest()
         {
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
             string printerName = "PRN-TEST-API";
             string printerNameNew = "PRN-TEST-API-2";
-            var result = _repository.RenamePrinter(printerName, printerNameNew);
+            var result = _repository.RenamePrinter(printerHostName, printerNameNew, propertyFilter);
             if (result == 10000u)
                 Assert.Fail("Expected printer {0} has not been found", printerName);
             else
             {
                 Assert.AreEqual(0, result);
-                _repository.RenamePrinter(printerNameNew, printerName);
+                _repository.RenamePrinter(printerHostName, printerName, propertyFilter);
             }
         }
 
         [Test()]
         public void ChangePropertyTest()
         {
-            string printerName = "PRN-TEST-API";
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
             string propertyName = "Location";
             string newValue = "Test Office-2";
             string initValue = "Test Office";
             ManagementObject mgmtObj = new ManagementObject();
 
-            var result = _repository.ChangeProperty(printerName, propertyName, newValue);
+            var result = _repository.ChangeProperty(printerHostName, propertyName, newValue, propertyFilter);
             if (String.IsNullOrEmpty(result.ToString()))
-                Assert.Fail("Printer's property {1} has already expected value", printerName, propertyName);
+                Assert.Fail("Printer's {0} property {1} has already expected value", printerHostName, propertyName);
             mgmtObj.Path = result;
             string resultPropertyValue = mgmtObj[propertyName].ToString();
 
             StringAssert.AreEqualIgnoringCase(resultPropertyValue, newValue);
 
-            _repository.ChangeProperty(printerName, propertyName, initValue);
+            _repository.ChangeProperty(printerHostName, propertyName, initValue, propertyFilter);
         }
 
         [Test()]
@@ -106,8 +110,10 @@ namespace PrintServerMgmtWebApi.Repository.Tests
         public void GetPrinterQueueTest()
         {
             string printerName = "PRN-TEST-API";
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
             string expectedJobName = "TestDocument.txt - Notepad";
-            ManagementObjectCollection jobCollection = _repository.GetPrinterQueue(printerName);
+            ManagementObjectCollection jobCollection = _repository.GetPrinterQueue(printerHostName, printerName, propertyFilter);
             List<string> jobNames = jobCollection
                 .Cast<ManagementObject>()
                 .ToList()
@@ -120,40 +126,43 @@ namespace PrintServerMgmtWebApi.Repository.Tests
         [Test()]
         public void ClearPrinterQueueTest()
         {
-            string printerName = "PRN-TEST-API";
-            uint result = _repository.ClearPrinterQueue(printerName);
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
+            uint result = _repository.ClearPrinterQueue(printerHostName, propertyFilter);
             Assert.AreEqual(0, result);
         }
 
         [Test()]
         public void AddPrintPermissionTest()
         {
-            string printerName = "PRN-TEST-API";
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
             string testSid = "S-1-5-32-550";
             string testName = "Print Operators";
-            var result = _repository.AddPrintPermission(printerName, testSid);
+            var result = _repository.AddPrintPermission(printerHostName, testSid, propertyFilter);
             if (result == 10001u)
                 Assert.Fail("Expected entity {0} already exists", testSid);
             else
             {
                 Assert.AreEqual(0, result);
-                _repository.RemoveFromACL(printerName, testName);
+                _repository.RemoveFromACL(printerHostName, testName, propertyFilter);
             }
         }
 
         [Test()]
         public void RemoveFromACLTest()
         {
-            string printerName = "PRN-TEST-API";
+            string printerHostName = "PRN-TEST-API";
+            string propertyFilter = "PortName";
             string testSid = "S-1-1-0";
             string testName = "Everyone";
-            var result = _repository.RemoveFromACL(printerName, testName);
+            var result = _repository.RemoveFromACL(printerHostName, testName, propertyFilter);
             if (result == 10000u)
                 Assert.Fail("No such entity {0}", testSid);
             else
             {
                 Assert.AreEqual(0, result);
-                _repository.AddPrintPermission(printerName, testSid);
+                _repository.AddPrintPermission(printerHostName, testName, propertyFilter);
             }
         }
     }
